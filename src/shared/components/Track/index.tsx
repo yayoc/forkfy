fc58e0a05d67159f5b2ebfbdccc8d385b0e00293
yayoc <n.yayoshi+github@gmail.com>
2017-09-11 20:47:35 +0900

    Adjust layout of track.

--- src/shared/components/Track/index.tsx
+++ src/shared/components/Track/index.tsx
@@ -10,7 +10,11 @@ export default (props: Track) => (
       <img src={props.album.images[0].url} width="45" height="45" />
     </div>
     <div className={`${grid["col-xs-9"]} ${s.content}`}>
-      <p className={s.name}>{props.name}</p>
+      <p className={s.name}>
+        <a href={props.href} target="_blank">
+          {props.name}
+        </a>
+      </p>
       <p className={s.artistName}>{props.artists.map(a => a.name).join(",")}</p>
     </div>
   </div>
