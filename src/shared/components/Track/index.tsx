c8b514a98b231a7acd4ef1c6658851f690d327b9
yayoc <n.yayoshi+github@gmail.com>
2017-09-11 20:27:42 +0900

    Add style for track component.

--- src/shared/components/Track/index.tsx
+++ src/shared/components/Track/index.tsx
@@ -1,10 +1,17 @@
 import * as React from "react";
 import { Track } from "shared/types";
 
+const grid = require("shared/assets/styles/flexboxgrid.min.css");
+const s = require("./Track.scss");
+
 export default (props: Track) => (
-  <div>
-    <h3>{props.name}</h3>
-    <p>{props.artists.map(a => a.name).join(",")}</p>
-    <img src={props.album.images[0].url} width="30" height="30" />
+  <div className={`${grid.row} ${s.track}`}>
+    <div className={`${grid["col-xs-offset-1"]}  ${grid["col-xs-2"]}`}>
+      <img src={props.album.images[0].url} width="45" height="45" />
+    </div>
+    <div className={`${grid["col-xs-9"]} ${s.content}`}>
+      <p className={s.name}>{props.name}</p>
+      <p className={s.artistName}>{props.artists.map(a => a.name).join(",")}</p>
+    </div>
   </div>
 );
