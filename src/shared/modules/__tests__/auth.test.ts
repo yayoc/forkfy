488b37574bab647528f772e3b6d46d5e21c806f3
yayoc <n.yayoshi+github@gmail.com>
2017-09-19 16:55:52 +0900

    Add action tests.

--- src/shared/modules/__tests__/auth.test.ts
+++ src/shared/modules/__tests__/auth.test.ts
@@ -2,6 +2,33 @@ import { ActionWithoutPayload, ActionWithPayload } from "shared/helpers/types";
 import { Me } from "shared/types";
 import reducer, { actions, Types } from "../auth";
 
+const me = {
+  birthdate: "1937-06-01",
+  country: "SE",
+  display_name: "JM Wizzler",
+  email: "email@example.com",
+  external_urls: {
+    spotify: "https://open.spotify.com/user/wizzler"
+  },
+  followers: {
+    href: null,
+    total: 3829
+  },
+  href: "https://api.spotify.com/v1/users/wizzler",
+  id: "wizzler",
+  images: [
+    {
+      height: null,
+      url:
+        "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-frc3/t1.0-1/1970403_10152215092574354_1798272330_n.jpg",
+      width: null
+    }
+  ],
+  product: "premium",
+  type: "user",
+  uri: "spotify:user:wizzler"
+};
+
 describe("auth module", () => {
   describe("actions", () => {
     it("should create a setting access token action.", () => {
@@ -17,6 +44,29 @@ describe("auth module", () => {
         type: Types.FETCH_ME_REQUEST
       };
       expect(actions.fetchMeRequest()).toEqual(expectedAction);
+    });
+    it("should create a succeed in a requesting me API action", () => {
+      const expectedAction = {
+        type: Types.FETCH_ME_SUCCESS,
+        payload: me
+      };
+      expect(actions.fetchMeSuccess(me)).toEqual(expectedAction);
+    })
+
+    it("should create a failure action", () => {
+      const error = new Error("")
+      const expectedAction = {
+        type: Types.FETCH_ME_FAILED,
+        payload: error
+      };
+      expect(actions.fetchMeFailed(error)).toEqual(expectedAction);
+    })
+
+    it("should create a logout action", () => {
+      const expectedAction = {
+        type: Types.LOGOUT
+      };
+      expect(actions.logout()).toEqual(expectedAction);
     });
   });
   describe("reducer", () => {
@@ -45,32 +95,7 @@ describe("auth module", () => {
       expect(reducer(undefined, action)).toEqual({ ...state, isLoading: true });
     });
     it("should return a state after getting me is succeeded", () => {
-      const me = {
-        birthdate: "1937-06-01",
-        country: "SE",
-        display_name: "JM Wizzler",
-        email: "email@example.com",
-        external_urls: {
-          spotify: "https://open.spotify.com/user/wizzler"
-        },
-        followers: {
-          href: null,
-          total: 3829
-        },
-        href: "https://api.spotify.com/v1/users/wizzler",
-        id: "wizzler",
-        images: [
-          {
-            height: null,
-            url:
-              "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-frc3/t1.0-1/1970403_10152215092574354_1798272330_n.jpg",
-            width: null
-          }
-        ],
-        product: "premium",
-        type: "user",
-        uri: "spotify:user:wizzler"
-      };
+
       const action: ActionWithPayload<Types.FETCH_ME_SUCCESS, Me> = {
         payload: me,
         type: Types.FETCH_ME_SUCCESS
